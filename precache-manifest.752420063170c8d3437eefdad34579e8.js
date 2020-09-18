722ebc1692c88c0472ec5910a2cbbd290509b94b
srdjansolajic <solajic.srdjan@gmail.com>
2020-09-18 11:58:43 +0200

    Updates

---
+++ precache-manifest.752420063170c8d3437eefdad34579e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee30adddccd31d5a66e2efe10d7b5c1f",
+    "url": "/SpaceX/index.html"
+  },
+  {
+    "revision": "1426fd512b42f49d6ca6",
+    "url": "/SpaceX/static/css/main.1626c29a.chunk.css"
+  },
+  {
+    "revision": "26288dc8f3beb3900f1c",
+    "url": "/SpaceX/static/js/2.1b296a4d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SpaceX/static/js/2.1b296a4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1426fd512b42f49d6ca6",
+    "url": "/SpaceX/static/js/main.83f64385.chunk.js"
+  },
+  {
+    "revision": "d4f30120dfedad64645b",
+    "url": "/SpaceX/static/js/runtime-main.4e3931ed.js"
+  }
+]);
